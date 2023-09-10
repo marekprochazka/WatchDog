4476e3497d23d6b56eddf42dda810ca535727f9c
Marek Procházka <m.prochazka2002@gmail.com>
2023-09-10 19:47:45 +0200

    ruff

--- services/backend/core/urls.py
+++ services/backend/core/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import json
 
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
